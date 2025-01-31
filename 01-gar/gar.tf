b02b3bc5432a0d4b94af5633fce65b64e9de32df
mamonaco1973 <mamonaco1973@gmail.com>
2025-01-31 22:43:33 +0000

    finalize backend

--- 01-gar/gar.tf
+++ 01-gar/gar.tf
@@ -4,4 +4,8 @@ resource "google_artifact_registry_repository" "flask_repo" {
   location      = "us-central1"
   repository_id = "flask-repository"
   format        = "DOCKER"
+  # Enable vulnerability scanning
+  docker_config {
+    immutable_tags = false
+  }
 }
