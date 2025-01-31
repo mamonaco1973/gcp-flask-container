22b7df7bd6c2db30253c31026ac0cdecc71704a6
mamonaco1973 <mamonaco1973@gmail.com>
2025-01-31 13:49:28 +0000

    tweak scripts

--- apply.sh
+++ apply.sh
@@ -6,9 +6,9 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Navigate to the 01-acr directory
-cd "01-acr" 
-echo "NOTE: Building ACR Repository."
+# Navigate to the 01-gar directory
+cd "01-gar" 
+echo "NOTE: Building GAR Repository."
 
 if [ ! -d ".terraform" ]; then
     terraform init
@@ -23,18 +23,18 @@ cd ..
 cd "02-docker"
 echo "NOTE: Building flask container with Docker."
 
-RESOURCE_GROUP="flask-container-rg"
-ACR_NAME=$(az acr list --resource-group $RESOURCE_GROUP --query "[?starts_with(name, 'flaskapp')].name | [0]" --output tsv)
-az acr login --name $ACR_NAME
-ACR_REPOSITORY="${ACR_NAME}.azurecr.io/flask-app"
-IMAGE_TAG="flask-app-rc1"
-docker build -t ${ACR_REPOSITORY}:${IMAGE_TAG} . --push
+#RESOURCE_GROUP="flask-container-rg"
+#ACR_NAME=$(az acr list --resource-group $RESOURCE_GROUP --query "[?starts_with(name, 'flaskapp')].name | [0]" --output tsv)
+#az acr login --name $ACR_NAME
+#ACR_REPOSITORY="${ACR_NAME}.azurecr.io/flask-app"
+#IMAGE_TAG="flask-app-rc1"
+#docker build -t ${ACR_REPOSITORY}:${IMAGE_TAG} . --push
 
 cd ..
 
-# Navigate to the 03-apprunner directory
-cd 03-containerapp
-echo "NOTE: Building container app instance."
+# Navigate to the 03-cloudrun directory
+cd 03-cloudrun
+echo "NOTE: Deploying flask container with cloud run."
 
 if [ ! -d ".terraform" ]; then
     terraform init
@@ -46,6 +46,6 @@ cd ..
 
 # Execute the validation script
 
-./validate.sh
+#./validate.sh
 
 
--- check_env.sh
+++ check_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "NOTE: Validating that required commands are found in your PATH."
+echo "NOTE: Validating that required commands are found in the PATH."
+
 # List of required commands
 commands=("gcloud" "docker" "terraform")
 
@@ -10,7 +11,7 @@ all_found=true
 # Iterate through each command and check if it's available
 for cmd in "${commands[@]}"; do
   if ! command -v "$cmd" &> /dev/null; then
-    echo "ERROR: $cmd is not found in the current PATH."
+    echo "ERROR: $cmd is not foundin the current PATH."
     all_found=false
   else
     echo "NOTE: $cmd is found in the current PATH."
@@ -25,47 +26,12 @@ else
   exit 1
 fi
 
-echo "NOTE: Validating that required environment variables are set."
-# Array of required environment variables
-required_vars=("ARM_CLIENT_ID" "ARM_CLIENT_SECRET" "ARM_SUBSCRIPTION_ID" "ARM_TENANT_ID")
+echo "NOTE: Validating credentials.json and test the gcloud command"
 
-# Flag to check if all variables are set
-all_set=true
-
-# Loop through the required variables and check if they are set
-for var in "${required_vars[@]}"; do
-  if [ -z "${!var}" ]; then
-    echo "ERROR: $var is not set or is empty."
-    all_set=false
-  else
-    echo "NOTE: $var is set."
-  fi
-done
-
-# Final status
-if [ "$all_set" = true ]; then
-  echo "NOTE: All required environment variables are set."
-else
-  echo "ERROR: One or more required environment variables are missing or empty."
+# Check if the file "./credentials.json" exists
+if [[ ! -f "./credentials.json" ]]; then
+  echo "ERROR: The file './credentials.json' does not exist." >&2
   exit 1
 fi
 
-echo "NOTE: Logging in to Azure using Service Principal..."
-az login --service-principal --username "$ARM_CLIENT_ID" --password "$ARM_CLIENT_SECRET" --tenant "$ARM_TENANT_ID" > /dev/null 2>&1
-
-# Check the return code of the login command
-if [ $? -ne 0 ]; then
-  echo "ERROR: Failed to log into Azure. Please check your credentials and environment variables."
-  exit 1
-else
-  echo "NOTE: Successfully logged into Azure."
-fi
-
-az provider register --namespace Microsoft.App
-
-while [[ "$(az provider show --namespace Microsoft.App --query "registrationState" --output tsv)" != "Registered" ]]; do
-  echo "NOTE: Waiting for Microsoft.App to register..."
-  sleep 10
-done
-echo "NOTE: Microsoft.App is currently registered!"
-
+gcloud auth activate-service-account --key-file="./credentials.json"
